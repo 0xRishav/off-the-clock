a2c36d3328e89971c7c2b505e3e8c2370623e34f
Rishav Bharti <64918176+iamrishavb@users.noreply.github.com>
2020-12-10 08:48:45 +0530

    feat: changed the entire UI Changed the entire UI this is a dark mode UI

--- src/App.js
+++ src/App.js
@@ -25,26 +25,50 @@ var travelKeys = Object.keys(travelObject);
 
 export default function App() {
   const [travelType, setTravelType] = useState("Weekend Break");
+  const [isActive, setActive] = useState({ active: "Weekend Break" });
+  const [bg, setBg] = useState("");
 
   function clickHandler(newTravelType) {
     setTravelType(newTravelType);
+    setActive({ active: newTravelType });
+  }
+  function getBg(index) {
+    if (index % 2 === 0) {
+      return {
+        bg:
+          "radial-gradient(33.05% 1230.22% at 50% 50%, rgba(112, 234, 154, 0.4) 0%, rgba(255, 255, 255, 0) 100%), #242625",
+
+        bgclr: "#70EA9A"
+      };
+    } else {
+      return {
+        bg:
+          "radial-gradient(33.05% 1230.22% at 50% 50%, rgba(174, 128, 34, 0.4) 0%, rgba(255, 255, 255, 0) 100%), #242625",
+        bgclr: "#DFB15B"
+      };
+    }
   }
 
   return (
     <div className="App">
-      <h1>Off The Clock✈️</h1>
-      <p style={{ marginBottom: "30px" }}>Travel Recommendation App</p>
+      <p>👋 Welcome to Off the Clock</p>
+      <h1>Off The Clock</h1>
+      <p style={{ marginBottom: "30px", fontSize: "14px", fontWeight: "bold" }}>
+        Off the Clock is a Travel Reccommendation app which gives you travel
+        reccommendation of diffrent types
+      </p>
       <div className="travel-types-wrapper">
         {travelKeys.map(function (place) {
           return (
             <span
-              key={place}
               onClick={() => clickHandler(place)}
+              className={isActive.active === place ? "active" : ""}
+              key={place}
               style={{
                 // margin: "30px 20px",
-                backgroundColor: "#E5E7EB",
-                padding: "0.5rem 0.8rem",
-                borderRadius: "0.2rem",
+                // backgroundColor: "#E5E7EB",
+                padding: "0.5rem 1rem",
+                borderRadius: "1rem",
                 outline: "none",
                 cursor: "pointer",
                 margin: "5px"
@@ -58,9 +82,25 @@ export default function App() {
       <div className="flex-div-cover">
         {travelObject[travelType].map(function (type, index) {
           return (
-            <div className="flex-div" key={index}>
-              <h4>{type.place}</h4>
-              <p>{type.rating}</p>
+            <div
+              className="flex-div"
+              key={index}
+              style={{ background: getBg(index).bg }}
+            >
+              <h4
+                style={{ color: "white", fontSize: "18px", fontWeight: "bold" }}
+              >
+                {type.place}
+              </h4>
+              <p
+                style={{
+                  color: getBg(index).bgclr,
+                  fontSize: "18px",
+                  fontWeight: "bold"
+                }}
+              >
+                {type.rating}
+              </p>
             </div>
           );
         })}
